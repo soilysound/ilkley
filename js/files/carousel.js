5e243718523f97170b566e4a97afcbbb96eb93fc
mark groak <markgroak@marks-MacBook.home>
2019-01-23 21:30:24 +0000

    add smooth scroll fall back

--- js/files/carousel.js
+++ js/files/carousel.js
@@ -1,5 +1,8 @@
 (function () {
 
+  'use strict';
+
+  var hassmoothscrolling = 'scrollBehavior' in document.documentElement.style;
   var carousels = document.querySelectorAll('[data-role="carousel"]');
 
   carousels.forEach(function (carousel) {
@@ -43,11 +46,97 @@
       requestAnimationFrame(tick);
     }
 
+    function smoothscroll(el, left, top) {
+
+      // aliases
+      var w = window;
+      var d = document;
+
+      var Element = w.HTMLElement || w.Element;
+      var SCROLL_TIME = 468;
+
+
+      // define timing method
+      var now =
+        w.performance && w.performance.now
+          ? w.performance.now.bind(w.performance)
+          : Date.now;
+
+      function ease(k) {
+        return 0.5 * (1 - Math.cos(Math.PI * k));
+      }
+
+      function step(context) {
+        var time = now();
+        var value;
+        var currentX;
+        var currentY;
+        var elapsed = (time - context.startTime) / SCROLL_TIME;
+
+        // avoid elapsed times higher than one
+        elapsed = elapsed > 1 ? 1 : elapsed;
+
+        // apply easing to elapsed time
+        value = ease(elapsed);
+
+        currentX = context.startX + (context.x - context.startX) * value;
+        currentY = context.startY + (context.y - context.startY) * value;
+
+        context.method.call(context.scrollable, currentX, currentY);
+
+        // scroll more if we have not reached our destination
+        if (currentX !== context.x || currentY !== context.y) {
+          w.requestAnimationFrame(step.bind(w, context));
+        }
+      }
+
+      function scrollElement(x, y) {
+        this.scrollLeft = x;
+        this.scrollTop = y;
+      }
+
+      function scroll(el, x, y) {
+        var scrollable;
+        var startX;
+        var startY;
+        var method;
+        var startTime = now();
+
+        // define scroll context
+
+        scrollable = el;
+        startX = el.scrollLeft;
+        startY = el.scrollTop;
+        method = scrollElement;
+
+
+        // scroll looping over a frame
+        step({
+          scrollable: scrollable,
+          method: method,
+          startTime: startTime,
+          startX: startX,
+          startY: startY,
+          x: x,
+          y: y
+        });
+      }
+
+      scroll(el, left, top);
+
+    }
+
     dotnav.onclick = function (event) {
       event.preventDefault();
       if (event.target.hasOwnProperty('navindex')) {
-        var left = slides[event.target.navindex].offsetLeft - (slides[event.target.navindex].offsetWidth / 2);
-        rail.scrollLeft = left;
+
+        var left = (slides[event.target.navindex].offsetLeft) - ((window.innerWidth - slides[event.target.navindex].offsetWidth) / 2);
+        if (hassmoothscrolling) {
+          rail.scrollLeft = left;
+        }
+        else {
+          smoothscroll(rail, left, 0)
+        }
       }
     }
 
